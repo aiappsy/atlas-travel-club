3b880082bbd29889a6189b75863762f6536f4743
Pål Alexander Juritzen <paljuritzen@gmail.com>
2026-09-11 06:56:50 +0200

    Disable aggressive caching in next.config.js

--- next.config.js
+++ next.config.js
@@ -12,6 +12,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'no-store, no-cache, must-revalidate, max-age=0',
+          },
+        ],
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
